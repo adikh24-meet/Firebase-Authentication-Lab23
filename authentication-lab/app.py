4b67274ad15fb451215079a6cd6ab1079050b135
adikh24-meet <139681014+adikh24-meet@users.noreply.github.com>
2023-07-24 16:21:11 +0300

    adi

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -11,9 +11,10 @@ config = {
   "storageBucket": "adi-signup.appspot.com",
   "messagingSenderId": "289458113899",
   "appId": "1:289458113899:web:0b89b30d21666e18e4502e",
-  "databaseURL":"" }
+  "databaseURL":"https://adi-signup-default-rtdb.europe-west1.firebasedatabase.app/" }
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
@@ -35,6 +36,12 @@ def signup():
         password = request.form['password']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            user = {
+            "full_name": request.form['full_name'], "email": request.form['email'],
+            "username": request.form['username'],"bio": request.form['bio']
+            }
+            db.child("Users").child(UID).set(user)
             return redirect(url_for('add_tweet'))
         except:
              error = "Authentication failed"
@@ -43,8 +50,22 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        Title = request.form['title']
+        text = request.form['text']
+        try:
+            tweet = {"title": request.form['title'], "text": request.form['text']}
+            db.child("tweets").child(UID).set(tweet)
+            return redirect(url_for('/all_tweets'))
+        except:
+             error = "tweet failed"
     return render_template("add_tweet.html")
 
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweets():
+    tweets = db.child("tweet").get().val()
+    return render_template("all_tweets.html", tweets=tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
